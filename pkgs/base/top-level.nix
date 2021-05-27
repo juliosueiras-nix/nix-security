c56c2e1f5ab64e1e3d2093524e2099fb9bc26a09
Julio Tain Sueiras <juliosueiras@gmail.com>
2021-05-27 00:35:39 -0400

    Update

--- pkgs/base/top-level.nix
+++ pkgs/base/top-level.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 
 {
-  inherit (pkgs) cifs-utils netkit-tftp iw lvm2 mlocate libressl nfs-utils openssh openvpn p7zip parted utillinux samba net-snmp sudo tcpdump testdisk tightvnc tmux unrar vim whois;
+  inherit (pkgs) cifs-utils netkittftp iw lvm2 mlocate libressl nfs-utils openssh openvpn p7zip parted utillinux samba net-snmp sudo tcpdump testdisk tightvnc tmux unrar vim whois;
 }
 
 
