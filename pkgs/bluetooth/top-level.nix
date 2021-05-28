d859c1ff2dfa27a388c4d8b5cb435912d0bddcee
Julio Tain Sueiras <juliosueiras@gmail.com>
2021-05-28 10:37:46 -0400

    'Added' bluez-hcidump

--- pkgs/bluetooth/top-level.nix
+++ pkgs/bluetooth/top-level.nix
@@ -3,7 +3,9 @@
 let
   oldBluez = callPackage ./oldBluez {};
 in {
+  inherit (pkgs) bluez;
   bluelog = callPackage ./bluelog { };
   blueranger = callPackage ./blueranger {  inherit oldBluez; };
   bluesnarfer = callPackage ./bluesnarfer { };
+  bluez-hcidump = oldBluez;
 }
