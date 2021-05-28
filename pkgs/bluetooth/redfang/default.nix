c424fa24938c89ad0636c5f9d880d66366c4265d
Julio Tain Sueiras <juliosueiras@gmail.com>
2021-05-28 11:10:51 -0400

    Added redfang

---
+++ pkgs/bluetooth/redfang/default.nix
@@ -0,0 +1,24 @@
+{ stdenv, makeWrapper, lib, fetchFromGitLab, bluez }:
+
+let
+  src = fetchFromGitLab {
+    owner = "kalilinux/packages";
+    repo = "redfang";
+    rev = "652d8999c9ef8e37e89c2ee7169179cadee73d67";
+    sha256 = "cDUhLHJTE7w5H1zBctndx8ClD0PC/kaEX+62YB6Gwkg=";
+  };
+in stdenv.mkDerivation {
+  name = "redfang";
+
+  inherit src;
+
+  patches = [
+    "${src}/debian/patches/Fix-format-security-error.patch"
+  ];
+
+  buildInputs = [ makeWrapper bluez ];
+
+  installPhase = ''
+    install -Dm755 fang $out/bin/fang
+  '';
+}
--- pkgs/bluetooth/top-level.nix
+++ pkgs/bluetooth/top-level.nix
@@ -3,10 +3,11 @@
 let
   oldBluez = callPackage ./oldBluez {};
 in {
-  inherit (pkgs) bluez;
+  inherit (pkgs) bluez crackle;
   bluelog = callPackage ./bluelog { };
   blueranger = callPackage ./blueranger {  inherit oldBluez; };
   bluesnarfer = callPackage ./bluesnarfer { };
   bluez-hcidump = oldBluez;
   btscanner = callPackage ./btscanner { };
+  redfang = callPackage ./redfang { };
 }
