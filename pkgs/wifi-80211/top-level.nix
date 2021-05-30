f7ddae2dc66d32971e6154a1f410ff9c4934699e
Julio Tain Sueiras <juliosueiras@gmail.com>
2021-05-29 20:21:22 -0400

    Update

--- pkgs/wifi-80211/top-level.nix
+++ pkgs/wifi-80211/top-level.nix
@@ -1,7 +1,7 @@
 { callPackage, pkgs, ... }:
 
 {
-  inherit (pkgs) aircrack-ng bully cowpatty mdk4;
+  inherit (pkgs) aircrack-ng bully cowpatty mdk4 hashcat iw kismet macchanger reaver wifite;
   asleap = callPackage ./asleap {}; 
   eapmd5pass = callPackage ./eapmd5pass {};
   fern-wifi-cracker = callPackage ./fern-wifi-cracker {};
