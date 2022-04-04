b2ed1c2ee7ec9f6f6a20e29e8928f4905bc69187
Julio Tain Sueiras <juliosueiras@gmail.com>
2022-04-04 04:06:59 -0400

    Testing

--- pkgs/top-level.nix
+++ pkgs/top-level.nix
@@ -8,7 +8,8 @@ let
   database = callPackage ./database/top-level.nix { };
   exploitation = callPackage ./exploitation/top-level.nix { };
   forensics = callPackage ./forensics/top-level.nix { };
-in (base // wifi-80211 // bluetooth // crypto-stego // database // exploitation // forensics // {
+in #(base // wifi-80211 // bluetooth // crypto-stego // database // exploitation // forensics // {
+  {
   "base.tightvnc" = base.tightvnc;
-  inherit base wifi-80211 bluetooth crypto-stego database exploitation forensics;
-})
+  #inherit base wifi-80211 bluetooth crypto-stego database exploitation forensics;
+}
