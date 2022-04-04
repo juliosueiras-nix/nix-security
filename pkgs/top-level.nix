d87256a31c7640c732e0391cf3974b3ad70b7ce3
Julio Tain Sueiras <juliosueiras@gmail.com>
2022-04-04 03:11:56 -0400

    Testing

--- pkgs/top-level.nix
+++ pkgs/top-level.nix
@@ -3,6 +3,7 @@
 {
   wifi-80211 = callPackage ./wifi-80211/top-level.nix { };
   base = callPackage ./base/top-level.nix { };
+  tightvnc = base.tightvnc;
   bluetooth = callPackage ./bluetooth/top-level.nix { };
   crypto-stego = callPackage ./crypto-stego/top-level.nix { };
   database = callPackage ./database/top-level.nix { };
