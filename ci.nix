429f1119c8f9b33730ae1f2d5b273841c999bb09
Julio Tain Sueiras <juliosueiras@gmail.com>
2022-04-10 20:31:18 -0400

    Update

--- ci.nix
+++ ci.nix
@@ -1,12 +1,3 @@
 let
   packages = (import (./.) { }).defaultNix.packages.${builtins.currentSystem};
-in {
-  base = packages.base; 
-  wifi-80211 = packages.wifi-80211;
-  bluetooth = packages.bluetooth;
-  crypto-stego = packages.crypto-stego;
-  database = packages.database;
-  exploitation = packages.exploitation;
-  forensics = packages.forensics;
-}
-
+in (packages.wifi-80211 // packages.base // packages.bluetooth // packages.database // packages.exploitation // packages.forensics)
